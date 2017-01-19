36efbc7bda57db923214fda3cbea4720140c76a4
mmj9@duke.edu <mmj9@duke.edu>
2017-01-18 19:46:21 -0500

    numero dos hecho

--- moss/002.c
+++ moss/002.c
@@ -3,8 +3,14 @@
 
 int main(int argc, char ** argv) {
   fprintf(stdout, "Finding the sum of all even Fibonacci numbers below 4 million\n");
-  int first = 1, second = 2, sum = 3;
-  
+  int first = 1, second = 2, sum = 2;
+  int next = first + second;  // 3
+  while (next < 4000000) {
+    first = second;
+    second = next;
+    next = first + second;
+    if (next % 2 == 0) sum += next;
+  }
   fprintf(stdout, "Sum = %d\n", sum);
   return EXIT_SUCCESS;
 }

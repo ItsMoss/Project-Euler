4040f91db3e5da5137ae2f5c36fbbcc6e945c310
Shu <sr278@duke>
2017-01-21 01:48:07 -0500

    a and b cannot be greater than 500

--- shu/Problem9abc.py
+++ shu/Problem9abc.py
@@ -5,8 +5,8 @@ Created on Sat Jan 21 01:34:32 2017
 @author: Shu
 """
 import math
-for b in range(1, 999):
-    for a in range(1, 999):
+for b in range(1, 500):
+    for a in range(1, 500):
         P = (1000 - b) * (1000 - a)
         if P == 500000  and b > a:
             abc = a*b*math.pow(b, 2)*math.pow(a, 2)

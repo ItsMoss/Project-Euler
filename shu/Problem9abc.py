82ed4488db8b348ea36315f3c5082d0006cec2ef
Shu <sr278@duke>
2017-01-21 01:49:12 -0500

    Removed line

--- shu/Problem9abc.py
+++ shu/Problem9abc.py
@@ -7,8 +7,7 @@ Created on Sat Jan 21 01:34:32 2017
 import math
 for b in range(1, 500):
     for a in range(1, 500):
-        P = (1000 - b) * (1000 - a)
-        if P == 500000  and b > a:
+        if (1000 - b) * (1000 - a) == 500000  and b > a:
             abc = a*b*math.pow(b, 2)*math.pow(a, 2)
             print(abc, a, b)
 
